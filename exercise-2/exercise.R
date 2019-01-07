2820b9bb3c7d159dc7a6e8651bf4a3b875e9568d
Van Tha Bik Lian <vanthabikl@gmail.com>
2019-01-06 18:26:18 -0800

    a

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -2,7 +2,7 @@
 
 # Create a vector `first_ten` that has the values 10 through 20 in it (using 
 # the : operator)
-first_ten < - 10:20
+first_ten <- 10:20
 
 # Create a vector `next_ten` that has the values 21 through 30 in it (using the 
 # seq() function)
@@ -24,25 +24,25 @@ even <- seq(2, 100, 2)
 
 # Using the `all()` function and `%%` (modulo) operator, confirm that all of the
 # numbers in your `even` vector are even
-
+all(even %% 2 == 0)
 
 
 # Create a vector `phone_numbers` that contains the numbers 8, 6, 7, 5, 3, 0, 9
-
+phone_numbers <- c(8, 6, 7, 5, 3, 0, 9)
 
 # Create a vector `prefix` that has the first three elements of `phone_numbers`
-
+prefix <- phone_numbers[1:3]
 
 # Create a vector `small` that has the values of `phone_numbers` that are 
 # less than or equal to 5
-
+small <- phone_numbers[phone_numbers <= 5]
 
 # Create a vector `large` that has the values of `phone_numbers` that are 
 # strictly greater than 5
-
+large <- phone_numbers[phone_numbers > 5]
 
 # Replace the values in `phone_numbers` that are larger than 5 with the number 5
-
+phone_numbers[phone_numbers > 5] <- 5
 
 # Replace every odd-numbered value in `phone_numbers` with the number 0
-
+phone_numbers[phone_numbers %% 2 != 0] <- 0
